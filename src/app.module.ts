4a5e6822773e961b80b1fab4d7f107a0febcc8f1
Andre Dorneles <andre@imperiosoft.com.br>
2023-06-20 10:19:53 -0300

    Fix NestJS ESM and Connecting Postgres with sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,43 +1,66 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+// Avoiding use await top level
+async function registerAdapterNest() {
+  const AdminJS = await import('adminjs');
+  const AdminJSSequelize = await import('@adminjs/sequelize');
+
+  AdminJS.default.registerAdapter({
+    Resource: AdminJSSequelize.Resource,
+    Database: AdminJSSequelize.Database,
+  });
+}
+registerAdapterNest();
 
 const DEFAULT_ADMIN = {
   email: 'admin@example.com',
   password: 'password',
-}
+};
 
 const authenticate = async (email: string, password: string) => {
   if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-    return Promise.resolve(DEFAULT_ADMIN)
+    return Promise.resolve(DEFAULT_ADMIN);
   }
-  return null
-}
+  return null;
+};
 
 @Module({
   imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'develop',
+      models: [],
+    }),
     // AdminJS version 7 is ESM-only. In order to import it, you have to use dynamic imports.
-    import('@adminjs/nestjs').then(({ AdminModule }) => AdminModule.createAdminAsync({
-      useFactory: () => ({
-        adminJsOptions: {
-          rootPath: '/admin',
-          resources: [],
-        },
-        auth: {
-          authenticate,
-          cookieName: 'adminjs',
-          cookiePassword: 'secret'
-        },
-        sessionOptions: {
-          resave: true,
-          saveUninitialized: true,
-          secret: 'secret'
-        },
+    import('@adminjs/nestjs').then(({ AdminModule }) =>
+      AdminModule.createAdminAsync({
+        useFactory: () => ({
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [],
+          },
+          auth: {
+            authenticate,
+            cookieName: 'adminjs',
+            cookiePassword: 'secret',
+          },
+          sessionOptions: {
+            resave: true,
+            saveUninitialized: true,
+            secret: 'secret',
+          },
+        }),
       }),
-    })),
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {}
