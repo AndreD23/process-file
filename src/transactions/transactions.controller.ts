38cd057f01dd8997f39e8831b0731f301cc28b16
Andre Dorneles <andre@imperiosoft.com.br>
2023-06-22 10:24:50 -0300

    Config upload with multer

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -6,17 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+const UPLOAD_DIR = './upload/transaction-files/';
+const MAX_UPLOAD_SIZE = 10; // MB
+
+const defaultConfig = diskStorage({
+  destination: UPLOAD_DIR,
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, 'file-' + uniqueSuffix);
+  },
+});
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
+  @UseInterceptors(FileInterceptor('file', { storage: defaultConfig }))
+  create(
+    @Body() createTransactionDto: CreateTransactionDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     return this.transactionsService.create(createTransactionDto);
   }
 
