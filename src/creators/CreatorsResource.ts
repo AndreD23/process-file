0043bd1d6d6cbe74bc45d5fddee5c14e73862b46
Andre Dorneles <andre@imperiosoft.com.br>
2023-06-24 13:14:45 -0300

    Arranging Items to Display in the Creators Panel

--- src/creators/CreatorsResource.ts
+++ src/creators/CreatorsResource.ts
@@ -4,5 +4,21 @@ export default {
   resource: Creator,
   options: {
     navigation: null,
+    properties: {
+      id: { position: 1 },
+      name: {
+        position: 2,
+        isRequired: true,
+      },
+      account_balance: {
+        position: 3,
+      },
+      createdAt: {
+        isVisible: false,
+      },
+      updatedAt: {
+        isVisible: false,
+      },
+    },
   },
 };
