3b42584eb38e7b4e4f4014ab233d0d5308754e23
Andre Dorneles <andre@imperiosoft.com.br>
2023-06-24 13:18:26 -0300

    Arranging Items to Display in the Transactions Panel

--- src/transactions/TransactionsResource.ts
+++ src/transactions/TransactionsResource.ts
@@ -4,5 +4,37 @@ export default {
   resource: Transaction,
   options: {
     navigation: 'Transactions',
+    properties: {
+      id: { position: 1 },
+      product: {
+        position: 2,
+        isRequired: true,
+      },
+      seller: {
+        position: 3,
+        isRequired: true,
+      },
+      value: {
+        position: 4,
+        isRequired: true,
+      },
+      type: {
+        position: 5,
+        isRequired: true,
+      },
+      data: {
+        position: 6,
+        isRequired: true,
+      },
+      transaction_file: {
+        position: 7,
+      },
+      createdAt: {
+        isVisible: false,
+      },
+      updatedAt: {
+        isVisible: false,
+      },
+    },
   },
 };
